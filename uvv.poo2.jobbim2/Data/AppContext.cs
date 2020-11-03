271745c4750a8b8b5dbb4b7304243e8a5ccc4b33
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-03 10:50:51 -0300

    setup ef core

---
+++ uvv.poo2.jobbim2/Data/AppContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ajj.Data {
+    public class AppContext : DbContext {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+    }
+}
