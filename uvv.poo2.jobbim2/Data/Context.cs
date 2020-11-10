719b284710efc4d00332bd6d6ffec2004ace3104
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-10 10:03:47 -0300

    create Employees table

--- uvv.poo2.jobbim2/Data/Context.cs
+++ uvv.poo2.jobbim2/Data/Context.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ajj.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ajj.Data {
     public class Context : DbContext {
+        public DbSet<Employee> Employees { get; set; }
+
         public Context(DbContextOptions<Context> options) : base(options) { }
     }
 }
---
+++ uvv.poo2.jobbim2/Migrations/20201110130305_CreateEmployeesTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ajj.Migrations
+{
+    public partial class CreateEmployeesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    AdmissionDate = table.Column<DateTime>(nullable: false),
+                    TerminationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
