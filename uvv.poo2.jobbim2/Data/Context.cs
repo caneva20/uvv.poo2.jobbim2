c70b4f5456db465e28634c055815a2c9667a7b36
Joao Guilherme <joao.g.f@icloud.com>
2020-11-10 10:10:44 -0300

    create courses table

--- uvv.poo2.jobbim2/Data/Context.cs
+++ uvv.poo2.jobbim2/Data/Context.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace ajj.Data {
     public class Context : DbContext {
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
         public Context(DbContextOptions<Context> options) : base(options) { }
     }
--- uvv.poo2.jobbim2/Models/Offer.cs
+++ uvv.poo2.jobbim2/Models/Offer.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 
 namespace ajj.Models {
+    public enum OfferStatus {
+        Closed,
+        Open,
+        Suspended,
+        Canceled,
+        Finished
+    }
     public class Offer {
         public long Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public Course Course { get; set; }
+        public OfferStatus Status { get; set; }
     }
+    
 }
