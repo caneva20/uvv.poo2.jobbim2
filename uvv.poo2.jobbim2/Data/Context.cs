40e69f8d435bdae8ab49e9d2794e343c0420c2a9
Joao Guilherme <joao.g.f@icloud.com>
2020-11-10 10:29:37 -0300

    create offers table

--- uvv.poo2.jobbim2/Data/Context.cs
+++ uvv.poo2.jobbim2/Data/Context.cs
@@ -5,6 +5,7 @@ namespace ajj.Data {
     public class Context : DbContext {
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Offer> Offers { get; set; }
 
         public Context(DbContextOptions<Context> options) : base(options) { }
     }
---
+++ uvv.poo2.jobbim2/Migrations/20201110132820_CreateOffersTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ajj.Migrations
+{
+    public partial class CreateOffersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    CourseId = table.Column<long>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offers_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_CourseId",
+                table: "Offers",
+                column: "CourseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offers");
+        }
+    }
+}
