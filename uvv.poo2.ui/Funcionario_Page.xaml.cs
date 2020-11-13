4d7a6f408f6064a2d39dd0c428de11546cd91964
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-13 07:30:59 -0300

    move ui to another project

---
+++ uvv.poo2.ui/Funcionario_Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Trabalho
+{
+    /// <summary>
+    /// Interação lógica para Funcionario_Page.xam
+    /// </summary>
+    public partial class Funcionario_Page : Page
+    {
+        public Funcionario_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
