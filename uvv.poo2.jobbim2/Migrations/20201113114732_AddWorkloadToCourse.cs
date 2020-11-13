08fc0d70c5c6a76a7af0451140f03b4e17bc4e95
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-13 08:48:04 -0300

    add workload to courses

---
+++ uvv.poo2.jobbim2/Migrations/20201113114732_AddWorkloadToCourse.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ajj.Migrations
+{
+    public partial class AddWorkloadToCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Workload",
+                table: "Courses",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Workload",
+                table: "Courses");
+        }
+    }
+}
--- uvv.poo2.jobbim2/Models/Course.cs
+++ uvv.poo2.jobbim2/Models/Course.cs
@@ -3,5 +3,6 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public string Program { get; set; }
+        public int Workload { get; set; }
     }
 }
