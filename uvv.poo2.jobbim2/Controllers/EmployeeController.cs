9bf1a58181b9e3bed532012b16f13156c22ebc18
Joao Guilherme <joao.g.f@icloud.com>
2020-11-17 11:55:14 -0300

    added employee controller

---
+++ uvv.poo2.jobbim2/Controllers/EmployeeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ajj.Data;
+using ajj.DTOs;
+using ajj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ajj.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public EmployeeController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetEmployees(long id)
+        {
+            var employee = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (employee == null)
+            {
+                return NotFound("Employee not found");
+            }
+
+            return employee;
+        }
+
+        [HttpGet("{id}")]
+        [ActionName("courses")]
+        public ActionResult<IEnumerable<Course>> GetEmployeesCourses(long id)
+        {
+            var employee = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (employee == null)
+            {
+                return NotFound("Employee not found");
+            }
+
+            var employeeCourses = new List<Course>();
+            foreach (var subscription in _context.Subscriptions.Where(x => x.Employee == employee))
+            {
+                employeeCourses.Add(subscription.Course);
+            }
+
+            return employeeCourses;
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee([FromBody] EmployeeDTO dto)
+        {
+            var employee = new Employee()
+            {
+                Name = dto.Name,
+                AdmissionDate = dto.AdmissionDate,
+            };
+
+            _context.Add(employee);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult TerminateContract(long id)
+        {
+            var employee = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (employee == null)
+            {
+                return NotFound("Employee not found");
+            }
+
+            employee.TerminationDate = DateTime.UtcNow;
+
+            _context.Update(employee);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ uvv.poo2.jobbim2/DTOs/EmployeeDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ajj.DTOs
+{
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public DateTime AdmissionDate { get; set; }
+    }
+}
