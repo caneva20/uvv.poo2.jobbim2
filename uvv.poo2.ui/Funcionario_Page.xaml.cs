309f72bfb7c9edcb09da49ca6cf56a3f995debc0
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-23 20:28:14 -0300

    add employee UI functionality

--- uvv.poo2.ui/Funcionario_Page.xaml.cs
+++ uvv.poo2.ui/Funcionario_Page.xaml.cs
@@ -1,58 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ajj;
 
-namespace WPF_Trabalho
-{
+namespace WPF_Trabalho {
     /// <summary>
     /// Interação lógica para Funcionario_Page.xam
     /// </summary>
-    public partial class Funcionario_Page : Page
-    {
+    public partial class Funcionario_Page : Page {
+        private readonly EmployeeClient _client;
+        
         string strNomeFuncionario;
         string strCargo;
         string strSetor;
         string strDataRecisao;
-        public Funcionario_Page()
-        {
+
+        public Funcionario_Page() {
             InitializeComponent();
+
+            _client = new EmployeeClient(new HttpClient());
+
+            LoadEmployees();
         }
 
-        private void criarFuncionario(object sender, RoutedEventArgs e)
-        {
-            strNomeFuncionario = input_Nome_Funcionario.Text;
-            strCargo = input_Cargo.Text;
-            strSetor = input_Setor.Text;
-            strDataRecisao = input_Data_Recisao.Text;
+        private async void LoadEmployees() {
+            var employees = await _client.GetEmployeesAsync();
+
+            foreach (var employee in employees) {
+                lista_funcionarios_cursos.Items.Add(employee);
+                lista_funcionarios_buscar.Items.Add(employee);
+                lista_funcionarios_encerrar.Items.Add(employee);
+            }
         }
 
-        private void listarCursos(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem typeItem = (ComboBoxItem)lista_funcionarios_cursos.SelectedItem;
-            string value = typeItem.Content.ToString();
+        private void CreateEmployee(object sender, RoutedEventArgs e) {
+            var name = input_Nome_Funcionario.Text;
+            
+            if (!DateTime.TryParse(input_Data_Admissao.Text, out var admissionDate)) {
+                admissionDate = DateTime.Now;
+            }
+
+            _client.CreateEmployeeAsync(new EmployeeDTO {
+                Name = name,
+                AdmissionDate = admissionDate
+            });
         }
 
-        private void listarDados(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem typeItem = (ComboBoxItem)lista_funcionarios_buscar.SelectedItem;
-            string value = typeItem.Content.ToString();
+        private async void ListCourses(object sender, RoutedEventArgs e) {
+            var employee = (Employee) lista_funcionarios_cursos.SelectedItem;
+
+            var courses = await _client.GetEmployeesCoursesAsync(employee.Id);
+
+            foreach (var course in courses) {
+                lista_Cursos.Items.Add(course);
+            }
         }
 
-        private void encerrarContrato(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem typeItem = (ComboBoxItem)lista_funcionarios_encerrar.SelectedItem;
-            string value = typeItem.Content.ToString();
+        private void ListInfo(object sender, RoutedEventArgs e) {
+            var employee = (Employee) lista_funcionarios_buscar.SelectedItem;
+
+            dados_Funcionario.Items.Clear();
+            dados_Funcionario.Items.Add($"Nome: {employee.Name}");
+            dados_Funcionario.Items.Add($"Data de admissão: {employee.AdmissionDate}");
+        }
+
+        private void Fire(object sender, RoutedEventArgs e) {
+            var employee = (Employee) lista_funcionarios_encerrar.SelectedItem;
+
+            _client.TerminateContractAsync(employee.Id);
         }
     }
 }
--- uvv.poo2.ui/Inscricao_Page.xaml.cs
+++ uvv.poo2.ui/Inscricao_Page.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,30 +13,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ajj;
 
-namespace WPF_Trabalho
-{
+namespace WPF_Trabalho {
     /// <summary>
     /// Interação lógica para Inscricao_Page.xam
     /// </summary>
-    public partial class Inscricao_Page : Page
-    {
-        public Inscricao_Page()
-        {
+    public partial class Inscricao_Page : Page {
+        private readonly EmployeeClient _client;
+        
+        public Inscricao_Page() {
             InitializeComponent();
+            
+            _client = new EmployeeClient(new HttpClient());
         }
 
-        private void criarInscricao(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem typeItemCurso = (ComboBoxItem)lista_cursos.SelectedItem;
-            ComboBoxItem typeItemFuncionario = (ComboBoxItem)lista_funcionarios.SelectedItem;
+        private async void criarInscricao(object sender, RoutedEventArgs e) {
+            ComboBoxItem typeItemCurso = (ComboBoxItem) lista_cursos.SelectedItem;
+            ComboBoxItem typeItemFuncionario = (ComboBoxItem) lista_funcionarios.SelectedItem;
             string curso = typeItemCurso.Content.ToString();
             string funcionario = typeItemFuncionario.Content.ToString();
+
+            foreach (var employee in await _client.GetEmployeesAsync()) {
+                lista_cursos.Items.Add(employee);
+            }
         }
 
-        private void cancelarInscricao(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem typeItem = (ComboBoxItem)lista_inscricao.SelectedItem;
+        private void cancelarInscricao(object sender, RoutedEventArgs e) {
+            ComboBoxItem typeItem = (ComboBoxItem) lista_inscricao.SelectedItem;
             string value = typeItem.Content.ToString();
         }
     }
